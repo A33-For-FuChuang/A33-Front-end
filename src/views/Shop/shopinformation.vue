40a65d3179189f2f52eb0cbd6f23d769c2245c0e
shilan <2933014340@qq.com>
2023-04-11 19:21:16 +0800

    完成添加商铺页面

--- src/views/Shop/shopinformation.vue
+++ src/views/Shop/shopinformation.vue
@@ -6,10 +6,17 @@
       <el-table-column prop="address" label="地址"></el-table-column>
       <el-table-column prop="size" label="商铺面积"></el-table-column>
     </el-table>
-    <br>
-    <el-button type="primary" @click="dialogFormVisible = true">添加商铺</el-button>
+    <br />
+    <el-button type="primary" @click="dialogFormVisible = true"
+      >添加商铺</el-button
+    >
     <el-dialog title="添加商铺" :visible.sync="dialogFormVisible">
-      <el-form :model="newShop" :rules="rules" ref="newShopForm" label-width="100px">
+      <el-form
+        :model="newShop"
+        :rules="rules"
+        ref="newShopForm"
+        label-width="100px"
+      >
         <el-form-item label="商铺名" prop="name">
           <el-input v-model="newShop.name"></el-input>
         </el-form-item>
@@ -29,57 +36,55 @@
 </template>
 
 <script>
-import { reqAllShop,reqAddShop } from "../../api/root";
-
+import { reqAllShop, reqAddShop } from "../../api/root";
+import { reqThreeMonthes, reqTest } from "@/api/location";
 export default {
+
   data() {
     return {
-      shops: [
-        
-      ],
+      shops: [],
       dialogFormVisible: false,
       newShop: {
-        name: '',
-        address: '',
-        size: ''
+        name: "",
+        address: "",
+        size: "",
       },
       rules: {
-        name: [{ required: true, message: '商铺名不能为空', trigger: 'blur' }],
-        address: [{ required: true, message: '地址不能为空', trigger: 'blur' }],
-        size: [{ required: true, message: '商铺面积不能为空', trigger: 'blur' }]
-      }
+        name: [{ required: true, message: "商铺名不能为空", trigger: "blur" }],
+        address: [{ required: true, message: "地址不能为空", trigger: "blur" }],
+        size: [
+          { required: true, message: "商铺面积不能为空", trigger: "blur" },
+        ],
+      },
     };
   },
-   created() {
+  created() {
     this.showstore();
   },
   methods: {
-   async  showstore()
-      {
-           const res=await reqAllShop()
-           console.log("这是商铺信息")
-           console.log(res)
-           this.shops=res.data
-      },
-   async addShop() {
-       console.log("这是添加商铺的信息1")
-          this.shops.push(this.newShop);
-          const res2=await reqAddShop(this.newShop)
-          console.log("这是添加商铺成功信息")
-          console.log(res2)
-          this.dialogFormVisible = false;
-          this.$message({
-            message: '添加成功',
-            type: 'success'
-          });
-          this.newShop = {
-            name: '',
-            address: '',
-            size: ''
-          };
-        
-    
-    }
-  }
+    async showstore() {
+      const res = await reqAllShop();
+      console.log("这是商铺信息");
+      console.log(res);
+      this.shops = res.data;
+    },
+    async addShop() {
+      console.log("这是添加商铺的信息1");
+      this.shops.push(this.newShop);
+      const res2 = await reqAddShop(this.newShop);
+      console.log("这是添加商铺成功信息");
+      console.log(res2);
+      this.dialogFormVisible = false;
+      this.$message({
+        message: "添加成功",
+        type: "success",
+      });
+      this.newShop = {
+        name: "",
+        address: "",
+        size: "",
+      };
+    },
+  },
 };
 </script>
